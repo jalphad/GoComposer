f72910d5f2bd9c07cd2b66d7024c44c2d458ade8
Joren Dirkse (YJ95TT) <joren.dirkse@ing.com>
2025-02-14 08:22:31 +0100

    Dependency method name change

--- workflows/task.go
+++ workflows/task.go
@@ -7,14 +7,14 @@ type Task interface {
 
 type Dependency[I any] interface {
 	Name() string
-	val(I)
+	ofType(I)
 }
 
 type dependency[O any] struct {
 	name string
 }
 
-func (d *dependency[O]) val(_ O) {}
+func (d *dependency[O]) ofType(_ O) {}
 
 func (d *dependency[O]) Name() string {
 	return d.name
@@ -26,6 +26,6 @@ func (w workflowInput[I]) Name() string {
 	return string(w)
 }
 
-func (w workflowInput[I]) val(_ I) {}
+func (w workflowInput[I]) ofType(_ I) {}
 
 const Input = "INPUT"
